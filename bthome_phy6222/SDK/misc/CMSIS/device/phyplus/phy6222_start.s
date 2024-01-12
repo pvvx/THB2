d036d51e67bf45d0dbdfa585d056d8fcd822f618
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2024-01-12 20:08:00 +0100

    stack, cosmetic

--- bthome_phy6222/SDK/misc/CMSIS/device/phyplus/phy6222_start.s
+++ bthome_phy6222/SDK/misc/CMSIS/device/phyplus/phy6222_start.s
@@ -11,7 +11,7 @@
 	.file		"phy6222_start.s"
 
 	/* *.ld: g_top_irqstack = ORIGIN(sram) + LENGTH(sram) */
-    .global g_top_irqstack
+    .global g_stack
 
 .text
 	.align	2
@@ -21,14 +21,14 @@
 	.type	__start, %function
 __start:
 
-	ldr     r1, = g_top_irqstack
+	ldr     r1, = g_irqstack_top
 	msr		msp, r1					/* r2>>sp */
 	bl		c_start				/* R0=IRQ, R1=register save area on stack */
 
 
 	.size	__start, .-__start
 
-
+#if 0 // Implemented in phy6222_vectors.c
 .section .isr_vector
     .align  4
     .globl  __Vectors
@@ -38,7 +38,17 @@ __Vectors:
     .long   __start
 
    .size   __Vectors, . - __Vectors
+#endif
 
+.section .irq_stack
+
+    .align  4
+    .global g_irqstack_base
+    .global g_irqstack_top
+
+g_irqstack_base:
+     .space 0x400
+g_irqstack_top:
 
 .end
 
