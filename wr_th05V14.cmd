3f604107e8cd3c7e8534a00e652ff4e777f38863
pvvx <pvvx@mail.ru>
2024-02-22 08:40:17 +0300

    v1.4b

--- wr_th05V14.cmd
+++ wr_th05V14.cmd
@@ -1,2 +1,2 @@
-python3 rdwr_phy62x2.py -p COM11 -e -r wh ./bin/BOOT_TH05_v13.hex
-@rem python3 rdwr_phy62x2.py -p COM11 -r we 0x10000 ./bin/TH05_v13.bin
+python3 rdwr_phy62x2.py -p COM11 -e -r wh ./bin/BOOT_TH05_v14.hex
+@rem python3 rdwr_phy62x2.py -p COM11 -r we 0x10000 ./bin/TH05_v14.bin
