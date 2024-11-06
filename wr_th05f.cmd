ba0601e1db3e640ad0377c9b7d1f5f6aff52bb95
pvvx <pvvx@mail.ru>
2024-11-06 11:12:09 +0300

    v1.9

--- wr_th05f.cmd
+++ wr_th05f.cmd
@@ -1,2 +1,2 @@
-python3 rdwr_phy62x2.py -p COM11 -e -r wh ./bin/BOOT_TH05F_v18.hex
-@rem python3 rdwr_phy62x2.py -p COM11 -r we 0x10000 ./bin/TH05F_v18.bin
+python3 rdwr_phy62x2.py -p COM11 -e -r wh ./bin/BOOT_TH05F_v19.hex
+@rem python3 rdwr_phy62x2.py -p COM11 -r we 0x10000 ./bin/TH05F_v19.bin
